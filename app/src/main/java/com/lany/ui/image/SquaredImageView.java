f8f050d139f5d4c624c91a4b361bd1f14a71c66a
Lany <lany192@qq.com>
2016-05-17 10:22:53 +0800

    添加SquaredImageView和SquaredFrameLayout

---
+++ app/src/main/java/com/lany/ui/image/SquaredImageView.java
@@ -0,0 +1,24 @@
+package com.lany.ui.image;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * 用于显示正方形ImageView
+ */
+public class SquaredImageView extends ImageView {
+    public SquaredImageView(Context context) {
+        super(context);
+    }
+
+    public SquaredImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
+    }
+}
---
+++ app/src/main/java/com/lany/ui/layout/SquareFrameLayout.java
@@ -0,0 +1,24 @@
+package com.lany.ui.layout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * 显示一个正方形发的FrameLayout
+ */
+public class SquareFrameLayout extends FrameLayout {
+    public SquareFrameLayout(Context context) {
+        super(context);
+    }
+
+    public SquareFrameLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
+    }
+}
